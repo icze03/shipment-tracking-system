74431928b8ebf314a52fe3f9a1ac66489ff69216
Mikhaela acel Esporlas <esporlasmikhaelaacel@gmail.com>
2025-12-30 20:05:22 +0000

    i also want a dark mode option in this app (_for element <Button>_)

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,11 +8,18 @@ export default {
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
+    container: {
+      center: true,
+      padding: '2rem',
+      screens: {
+        '2xl': '1400px',
+      },
+    },
     extend: {
       fontFamily: {
-        body: ['Inter', 'sans-serif'],
-        headline: ['Inter', 'sans-serif'],
-        code: ['monospace'],
+        sans: ['var(--font-sans)'],
+        mono: ['var(--font-mono)'],
+        heading: ['var(--font-heading)'],
       },
       colors: {
         background: 'hsl(var(--background))',
