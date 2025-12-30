6367a7ee3c67f9040c0c4849bd19249b52c8af33
Mikhaela acel Esporlas <esporlasmikhaelaacel@gmail.com>
2025-12-30 20:22:22 +0000

    remove this add driver button in the driver management  since theres a s

--- src/app/admin/drivers/page.tsx
+++ src/app/admin/drivers/page.tsx
@@ -1,6 +1,4 @@
-import { PlusCircle } from "lucide-react";
-import Link from "next/link";
-import { Button } from "@/components/ui/button";
+
 import { getDrivers } from "@/lib/data/drivers";
 import { DriverDataTable } from "@/components/admin/driver-data-table";
 import { columns } from "@/components/admin/driver-columns";
@@ -14,16 +12,8 @@ export default async function DriversPage() {
         <div>
           <h2 className="text-2xl font-bold tracking-tight font-headline">Driver Management</h2>
           <p className="text-muted-foreground">
-            Add, view, and manage your team of drivers.
+            View and manage your team of drivers.
           </p>
-        </div>
-        <div className="flex-shrink-0">
-          <Button asChild>
-            <Link href="/admin/drivers/add">
-              <PlusCircle className="mr-2 h-4 w-4" />
-              Add Driver
-            </Link>
-          </Button>
         </div>
       </div>
       <DriverDataTable columns={columns} data={drivers} />
