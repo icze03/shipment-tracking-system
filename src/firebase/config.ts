6a5a35b44c3caea9172b8afd89636a5549c927fb
Mikhaela acel Esporlas <esporlasmikhaelaacel@gmail.com>
2025-12-30 17:55:26 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-6239242929-ea269",
+  "appId": "1:213378512084:web:bc461cb0b02c81623fb741",
+  "apiKey": "AIzaSyCvVisgi23GBoQLMgMLhiRIqm-12p5rW3E",
+  "authDomain": "studio-6239242929-ea269.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "213378512084"
+};
